c14bacbab8b55b1ebb21f858f26802dfc0344a4e
acbuie <aidancbuie@gmail.com>
2021-03-19 10:49:12 -0600

    update default parameters

--- parameters.py
+++ parameters.py
@@ -7,6 +7,6 @@ N = 1000 # Number of points/precision
 CONTOUR_COLORS = ['blue', 'yellow'] # Contour color gradient
 INTERSECTION_COLOR = ['black', 0.75] # Intersection color and alpha
 
-SAVE = False # Save plot?
+SAVE = True # Save plot?
 SAVE_NAME = 'stress_plot.png' # Filename
 SAVE_LOC = f'./{SAVE_NAME}' # Default save loc is current directory
